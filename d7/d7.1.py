2e2a4f81e036cec63a2ca1a51cea1d5b37bd63ac
Jean-Marc Montanier <jmmontanier@aldebaran.com>
2016-12-08 15:49:56 +0100

    ok pour d7.1

--- d7/d7.1.py
+++ d7/d7.1.py
@@ -7,22 +7,29 @@ import heapq
 def checkAbba(strings):
     oneStringOk = False
     for string in strings:
-        thisStringOk = True
-        for idL in range(len(string)-4):
+        thisStringOk = False
+        for idL in range(len(string)-3):
             if string[idL] != string[idL+1] and string[idL] == string[idL+3] and string[idL+1] == string[idL+2]:
-            
+                thisStringOk = True
+                break
+        if thisStringOk == True:
+            oneStringOk = True
+            break
+    return oneStringOk
 
 if __name__ == "__main__":
     fo = open(sys.argv[1],"r")
     lines = fo.readlines()
 
+    cpt = 0
     for line in lines:
         line = line.strip()
         print(line)
         #get the hypernets
         hypernets = re.findall("\[([a-z]+)\]",line)
-        print(hypernets)
         #get the rest
         strings = re.sub(r"\[[a-z]+\]",".",line).split(".")
-        print(strings)
-
+        if checkAbba(hypernets)  == False and checkAbba(strings) == True:
+            print ("valid")
+            cpt += 1
+    print(cpt)
